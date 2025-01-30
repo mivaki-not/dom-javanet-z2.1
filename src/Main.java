f0be8d6f95596221e57e1930da21e8b4ac78609e
MIVAKI <elenaananenko2605@gmail.com>
2025-01-30 19:50:06 +0300

    Added commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        int bilet = 2;
         int price = 12000; // стоимости билета
-        int mili = 20; // рублей для одной бонусной мили
+        int miliPrice = 20; // рублей для одной бонусной мили
 
-        System.out.println((bilet * price ) + " Цена за два билета ");
-        System.out.println(((bilet * (price / mili)) + " Получено бонусов "));
+        System.out.println( " Получено ");
+        System.out.println((price / miliPrice) + " бонусов ");
 
     }
 }
